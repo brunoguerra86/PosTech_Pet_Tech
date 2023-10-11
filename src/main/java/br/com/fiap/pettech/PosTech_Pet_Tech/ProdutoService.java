fce2b2cb191c91862e3de81829a5ee49a9e2f86e
brunoguerra <brunoguerra@hotmail.com>
2023-10-11 07:57:47 -0300

    feat: tratamento de erros

--- src/main/java/br/com/fiap/pettech/PosTech_Pet_Tech/ProdutoService.java
+++ src/main/java/br/com/fiap/pettech/PosTech_Pet_Tech/ProdutoService.java
@@ -1,5 +1,6 @@
 package br.com.fiap.pettech.PosTech_Pet_Tech;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,8 @@ public class ProdutoService {
         return produtos;
     }
 
-    public Optional<Produto> findById(UUID id){
-        var produto = repo.findById(id);
+    public Produto findById(UUID id){
+        var produto = repo.findById(id).orElseThrow(() -> new ControllerNotFoundException("Produto não encontrado"));
         return produto;
     }
 
@@ -29,14 +30,20 @@ public class ProdutoService {
     }
 
     public Produto update(UUID id, Produto produto){
-        Produto buscaProduto = repo.getOne(id);
-        buscaProduto.setNome(produto.getNome());
-        buscaProduto.setDescricao(produto.getDescricao());
-        buscaProduto.setUrlDaImagem(produto.getUrlDaImagem());
-        buscaProduto.setPreco(produto.getPreco());
-        buscaProduto = repo.save(buscaProduto);
 
-        return buscaProduto;
+        try {
+            Produto buscaProduto = repo.getOne(id);
+            buscaProduto.setNome(produto.getNome());
+            buscaProduto.setDescricao(produto.getDescricao());
+            buscaProduto.setUrlDaImagem(produto.getUrlDaImagem());
+            buscaProduto.setPreco(produto.getPreco());
+            buscaProduto = repo.save(buscaProduto);
+
+            return buscaProduto;
+        } catch (EntityNotFoundException e){
+            throw new ControllerNotFoundException("Produto não encontrado");
+        }
+
     }
 
     public void delete(UUID id){
